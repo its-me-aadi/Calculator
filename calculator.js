0f08d228960ae6b13179509b3111c16b61592b1b
Aditya Sharma <itsadityasharma7124@gmail.com>
2023-02-07 17:20:36 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -4,16 +4,15 @@ const app=express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.get("/",function(request,response){
     response.sendFile(__dirname+ "/calculator.html");
-})
-app.get("/about",function(req,res){
-    res.send("jai shree ram");
-})
+});
+
 app.post("/",function(req,resp){
     var num1=parseInt(req.body.num1);
     var num2=parseInt(req.body.num2);
     var sum=num1+num2
-    resp.send("post ho gya bhadwe " + sum);
-})
+    resp.send(sum);
+});
+
 app.listen(3000,function(){
     console.log("Server started");
 });  
